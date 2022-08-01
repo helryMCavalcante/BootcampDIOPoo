3f94f1a96bfa09dced11e8c62d165e372275d7d9
HelryMCavalcanteC <helrycavalcante@gmail.com>
2022-08-01 20:03:39 -0300

    Curso e Mentoria

---
+++ src/com/company/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,38 @@
+package com.company.br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+
+    private String titulo;
+    private String descriacao;
+    private LocalDate data;
+
+    public Mentoria(){
+
+  }
+
+    public String getTitulo() {
+    return titulo;
+  }
+
+    public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+    public String getDescriacao() {
+    return descriacao;
+  }
+
+    public void setDescriacao(String descriacao) {
+    this.descriacao = descriacao;
+  }
+
+     public LocalDate getData() {
+    return data;
+  }
+
+    public void setData(LocalDate data) {
+    this.data = data;
+  }
+}
