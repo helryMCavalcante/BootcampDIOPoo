8fad34b2e3479d797b2455f2a1bb45ca78eb528d
HelryMCavalcanteC <helrycavalcante@gmail.com>
2022-08-03 15:00:54 -0300

    Regras de negocio implementadas.

--- src/com/company/br/com/dio/desafio/dominio/Dev.java
+++ src/com/company/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package com.company.br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,11 +11,26 @@ public class Dev {
     private Set<Conteudo> conteudosIncritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp (BootCamp bootCamp){}
+    public void inscreverBootcamp (BootCamp bootCamp){
+        this.conteudosIncritos.addAll(bootCamp.getConteudos());
+        bootCamp.getDevsInscritos().add(this);
+    }
 
-    public void progredir(){}
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosIncritos.stream().findFirst();
+        if (conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosIncritos.remove(conteudo.get());
+        }else{
+            System.err.println("Você não esta matriculado em nenhum curso.");
+        }
+    }
 
-    public void calcularTotalXp(){}
+    public double calcularTotalXp(){
+        this.conteudosConcluidos.stream()
+                .mapToDouble(conteudo ->conteudo.calcularXp())
+                .sum();
+    }
 
     public String getNome() {
         return nome;
